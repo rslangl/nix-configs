ddb3b2a56422b5b9a4e2538ca25c78f73a336e24
rslangl <fjellape1447@protonmail.com>
2025-02-09 15:12:10 +0100

    feat: add previous ohmyzsh aliases

--- home.nix
+++ home.nix
@@ -102,6 +102,10 @@
       ls="ls --color=auto";
       la="ls -la";
       ll="ls -l";
+      wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts";
+      sbt="sbt -ivy $XDG_DATA_HOME/ivy2 -sbt-dir $XDG_DATA_HOME/sbt";
+      mvn="mvn -gs $XDG_CONFIG_HOME/maven/settings.xml";
+      nvidia-settings="nvidia-settings --config=$XDG_CONFIG_HOME/nvidia/settings";
     };
   };
 
