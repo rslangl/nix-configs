3fea117c1f627d650a9c0d7b04042a26346d348a
rslangl <fjellape1447@protonmail.com>
2025-02-09 15:10:09 +0100

    fix: typo for hyprland cocfg

--- home.nix
+++ home.nix
@@ -119,7 +119,7 @@
     ] ++ (
       # workspaces
       # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-      builtins.concatList (builtins.genList (i:
+      builtins.concatLists (builtins.genList (i:
         let ws = i + 1;
 	in [
 	    "$mod, code:1${toString i}, workspace, ${toString ws}"
