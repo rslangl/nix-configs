8a3977dfdb032a4918dedb32d4666a0d1e75576c
rslangl <fjellape1447@protonmail.com>
2025-02-23 16:14:21 +0100

    feat: add fontawesome

--- configuration.nix
+++ configuration.nix
@@ -144,6 +144,10 @@
     dconf
   ];
 
+  fonts.packages = with pkgs; [
+    font-awesome
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
