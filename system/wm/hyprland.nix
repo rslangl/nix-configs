cd589e6df969e709b91af9f7524444982403f7d4
rslangl <fjellape1447@protonmail.com>
2025-09-14 17:23:50 +0200

    chore: enable SDDM

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -20,4 +20,14 @@ in
   };
 
   services.xserver.excludePackages = [ pkgs.xterm ];
+
+   services.xserver = {
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      enableHidpi = true;
+      theme = "chili";
+      package = pkgs.sddm;
+    };
+  };
 }
