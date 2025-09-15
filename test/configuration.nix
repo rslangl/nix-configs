a2bd198743268b04aaf639dfd15aef963594d702
rslangl <fjellape1447@protonmail.com>
2025-09-15 22:12:52 +0200

    chore: use real flake and host configs for test environemtn

--- test/configuration.nix
+++ test/configuration.nix
@@ -2,28 +2,33 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix ];
-
-  system.stateVersion = "25.05";
-
-  users.users.vmuser = {
-    isNormalUser = true;
-    password = "vmuser"; 
-    extraGroups = [ "wheel" ];
-  };
-
-  services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  services.openssh.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    firefox
-    kate
-    xterm
-    git
+  imports = [
+    #./hardware-configuration.nix
+    ../profiles/personal/configuration.nix
   ];
+
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.grub.enable = lib.mkForce false;
+  boot.extraModprobeConfig = lib.mkForce "";  # NOTE: may use either mkOverride or mkForce to override from real configuration.nix
+  boot.loader.generic-extlinux-compatible.enable = lib.mkForce false;
+  boot.isContainer = true;
+
+  # users.users.vmuser = {
+  #   isNormalUser = true;
+  #   password = "vmuser"; 
+  #   extraGroups = [ "wheel" ];
+  # };
+  #
+  # services.xserver.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+
+  # environment.systemPackages = with pkgs; [
+  #   firefox
+  #   kate
+  #   xterm
+  #   git
+  # ];
 
   networking.networkmanager.enable = true;
 
