ba5e5ea724faa9ef7c0d3204aa9ccfff11757657
rslangl <fjellape1447@protonmail.com>
2025-09-25 22:17:10 +0200

    chore: fix test structure

---
+++ test/README.md
@@ -0,0 +1,68 @@
+# Test
+
+VMs for testing the NixOS configs.
+
+## Usage
+
+Run VM:
+```shell
+./run-vm.sh (debian|debiancloud|nixos)
+```
+
+This will launch the VM with a VNC connection available at `:5901`. Access it using e.g.:
+```shell
+nix-shell -p vnctiger
+vncviewer localhost:5901
+```
+
+## Building
+
+### Option 1: Conventional image
+
+We can build a bootable image by providing a regular ISO that creates a virtual drive file:
+```shell
+qemu-img create -f qcow2 debian_drive.qcow2 20G
+qemu-system-x86_64 \
+  -cdrom debian.iso \
+  -drive file=debian_drive.qcow2,format=qcow2 \
+  -boot c \
+  -m 4G \
+  -enable-kvm \
+  -cpu host \
+  -smp 4 \
+  -device virtio-net-pci,netdev=net0 \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -vga virtio \
+  -display none \
+  -vnc :1
+```
+
+Then access the VM over VNC and follow the install steps. Once installed, remove the `-cdrom` argument and set `-boot d`.
+
+### Option 2: Cloud image
+
+Cloud images rely on a seed image to do system initialization. These are in turn relying on certain user input, such as `user-data` and `meta-data`:
+```shell
+genisoimage -output seed.iso -volid cidata -joliet -rock cloud-init/user-data cloud-init/meta-data
+```
+
+With the generated image, run the VM with VNC:
+```shell
+qemu-system-x86_64 \
+  -drive file=debian_nocloud.qcow2,format=qcow2 \
+  -drive file=seed.iso,format=raw \
+  -boot d \
+  -m 4G \
+  -enable-kvm \
+  -cpu host \
+  -smp 4 \
+  -device virtio-net-pci,netdev=net0 \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -vga virtio \
+  -display none \
+  -vnc :1
+```
+```
+```
+```
+
--- test/run-vm.sh
+++ test/run-vm.sh
@@ -1,85 +1,109 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Root and test directories
-ROOT_DIR="$(dirname "$(realpath "$0")")"
-TEST_DIR="$ROOT_DIR/test"
-IMG_NAME="nixos-25.05-x86_64-linux.qcow2"
-IMG_PATH="$TEST_DIR/$IMG_NAME"
-SEED_ISO="$TEST_DIR/seed.iso"
-USER_DATA="$TEST_DIR/user-data.yaml"
-META_DATA="$TEST_DIR/meta-data"
+# TODO: use associative array for resources
 
-# Your repo and setup script inside VM
-GITHUB_REPO="https://github.com/youruser/yourrepo.git"
-SETUP_SCRIPT_PATH="install.sh"  # relative path inside yourrepo
+DEBIAN_CLOUD_ISO_URL="https://cloud.debian.org/images/cloud/trixie/20250924-2245/debian-13-nocloud-amd64-20250924-2245.qcow2"
+DEBIAN_CLOUD_ISO="debian13_nocloud.qcow2"
+DEBIAN_CLOUD_SEED_ISO="debian13_nocloud_seed.iso"
 
-# NixOS cloud image URL
-NIXOS_CLOUD_IMAGE_URL="https://releases.nixos.org/nixos/25.05/nixos-25.05-x86_64-linux.qcow2"
+DEBIAN_ISO_URL="http://ftp.uio.no/debian-cd/13.1.0/amd64/iso-cd/debian-13.1.0-amd64-netinst.iso"DEBIAN_ISO="debian13.iso"
+DEBIAN_ISO="debian13.iso"
+DEBIAN_DRIVE_FILE="debian.qcow2"
 
-mkdir -p "$TEST_DIR"
+NIXOS_ISO_URL="https://channels.nixos.org/nixos-25.05/latest-nixos-graphical-x86_64-linux.iso"
+NIXOS_ISO="nixos.iso"
+NIXOS_DRIVE_FILE="nixos.qcow2"
 
-echo "Checking if image exists at $IMG_PATH..."
-if [[ ! -f "$IMG_PATH" ]]; then
-  echo "Image not found. Downloading NixOS cloud image..."
-  curl -L -o "$IMG_PATH" "$NIXOS_CLOUD_IMAGE_URL"
-else
-  echo "Image already exists."
+if [[ ! -f "$DEBIAN_ISO" ]]; then
+  echo "Downloading Debian ISO..."
+  curl -Lo "$DEBIAN_ISO" "$DEBIAN_ISO_URL"
 fi
 
-echo "Generating cloud-init user-data file..."
-cat > "$USER_DATA" <<EOF
-#cloud-config
-runcmd:
-  - [ sh, -c, "git clone $GITHUB_REPO /root/setup || (cd /root/setup && git pull)" ]
-  - [ sh, -c, "chmod +x /root/setup/$SETUP_SCRIPT_PATH && /root/setup/$SETUP_SCRIPT_PATH" ]
-EOF
-
-# Empty meta-data file required for cloud-init seed ISO
-touch "$META_DATA"
-
-# Function to create seed.iso using Docker with cloud-localds
-create_seed_iso_with_docker() {
-  echo "Creating seed ISO using Docker container..."
-  docker run --rm -v "$TEST_DIR":/workdir ubuntu:22.04 bash -c "
-    apt update -qq && apt install -y -qq cloud-image-utils && \
-    cloud-localds /workdir/seed.iso /workdir/user-data.yaml
-  "
-}
-
-# Function to create seed.iso using genisoimage fallback
-create_seed_iso_with_genisoimage() {
-  echo "Docker not available or failed. Creating seed ISO using genisoimage..."
-  nix-shell -p cdrtools --run "genisoimage -output '$SEED_ISO' -volid cidata -joliet -rock '$USER_DATA' '$META_DATA'"
-}
-
-echo "Creating seed ISO for cloud-init..."
-
-if command -v docker >/dev/null 2>&1; then
-  if create_seed_iso_with_docker; then
-    echo "Seed ISO created with Docker."
-  else
-    echo "Docker failed to create seed ISO. Falling back to genisoimage."
-    create_seed_iso_with_genisoimage
-  fi
-else
-  echo "Docker not found. Using genisoimage to create seed ISO."
-  create_seed_iso_with_genisoimage
+if [[ ! -f "$DEBIAN_CLOUD_ISO" ]]; then
+  echo "Downloading Debian cloud ISO..."
+  curl -Lo "$DEBIAN_CLOUD_ISO" "$DEBIAN_CLOUD_ISO_URL"
 fi
 
-echo "Launching QEMU..."
+if [[ ! -f "$NIXOS_ISO" ]]; then
+  echo "Downloading NixOS ISO..."
+  curl -Lo "$NIXOS_ISO" "$NIXOS_ISO_URL"
+fi
 
-qemu-system-x86_64 \
-  -drive file="$IMG_PATH",format=raw \
-  -drive file="$SEED_ISO",format=raw,if=virtio \
-  -m 4G \
-  -enable-kvm \
-  -cpu host \
-  -smp 4 \
-  -device virtio-net,netdev=net0 \
-  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
-  -vga qxl \
-  -display gtk
+if [[ ! -f "$DEBIAN_DRIVE_FILE" ]]; then
+  echo "Creating Debian drive file..."
+  qemu-img create -f qcow2 "$DEBIAN_DRIVE_FILE" 20G
+fi
 
-echo "VM exited."
+if [[ ! -f "$NIXOS_DRIVE_FILE" ]]; then
+  echo "Creating NixOS drive file..."
+  qemu-img create -f qcow2 "$NIXOS_DRIVE_FILE" 20G
+fi
+
+# TODO: check args
+
+SYSTEM="$1"
+TIMESTAMP=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+
+# TODO: wrap calls to functions for specific conditions:
+# build drive file require both ISO and drive file, `boot c`
+# boot from drive file require only drive file, `boot d`
+# booting from cloud image require a seed image to be created first
+
+case "$SYSTEM" in
+  debian)
+    echo "Making snapshot prior to launching..."
+    qemu-img create -f qcow2 -b "$DEBIAN_DRIVE_FILE" -F qcow2 "debian_snapshot_${TIMESTAMP}.qcow2"
+    echo "Launching Debian.."
+    qemu-system-x86_64 \
+      -cdrom "$DEBIAN_ISO" \
+      -drive file="$DEBIAN_DRIVE_FILE",format=qcow2 \
+      -boot c \
+      -m 4G \
+      -enable-kvm \
+      -cpu host \
+      -smp 4 \
+      -device virtio-net-pci,netdev=net0 \
+      -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+      -vga virtio \
+      -display none \
+      -vnc :1
+  ;;
+  debiancloud)
+    echo "Launching Debian cloud..."
+    qemu-system-x86_64 \
+      -drive file="$DEBIAN_CLOUD_ISO",format=qcow2 \
+      -drive file="$DEBIAN_CLOUD_SEED_ISO",format=raw \
+      -boot d \
+      -m 4G \
+      -enable-kvm \
+      -cpu host \
+      -smp 4 \
+      -device virtio-net-pci,netdev=net0 \
+      -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+      -vga virtio \
+      -display none \
+      -vnc :1
+  ;;
+  nixos)
+    echo "Launching NixOS.."
+    qemu-system-x86_64 \
+      -cdrom "$NIXOS_ISO" \
+      -drive file="$NIXOS_DRIVE_FILE",format=qcow2 \
+      -boot d \
+      -m 4G \
+      -enable-kvm \
+      -cpu host \
+      -smp 4 \
+      -device virtio-net-pci,netdev=net0 \
+      -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+      -vga virtio \
+      -display none \
+      -vnc :1
+  ;;
+  *)
+    echo "No such system: $SYSTEM"
+    exit 1
+  ;;
+esac
 
