05b68f878545a138b6ae7939cf9a63e1b8da12c5
rslangl <fjellape1447@protonmail.com>
2025-09-30 19:57:14 +0200

    chore: specify XDG base dirs globally

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -60,6 +60,18 @@
     uid = 1000;
   };
 
+  environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     vim
     wget
