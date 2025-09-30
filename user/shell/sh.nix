c0e8279ddd71e7b11b2140e83db0aa416a18af02
rslangl <fjellape1447@protonmail.com>
2025-09-30 20:42:09 +0200

    chore: add XDG support for selected applications

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -8,12 +8,12 @@
     oh-my-zsh
   ];
 
-  # xdg.configFile."npm/npmrc" = ''
-  #   prefix=$XDG_DATA_HOME/npm
-  #   cache=$XDG_CACHE_HOME/npm
-  #   init-module=$XDG_CONFIG_HOME/npm/config/npm-init.js
-  #   logs-dir=$XDG_STATE_HOME/npm/logs
-  # '';
+  xdg.configFile."npm/npmrc".text = ''
+    prefix=$XDG_DATA_HOME/npm
+    cache=$XDG_CACHE_HOME/npm
+    init-module=$XDG_CONFIG_HOME/npm/config/npm-init.js
+    logs-dir=$XDG_STATE_HOME/npm/logs
+  '';
 
   programs.zsh = {
     enable = true;
@@ -52,8 +52,22 @@
     envExtra = "
       export RUSTUP_HOME=${config.xdg.dataHome}/rustup\n
       export CARGO_HOME=${config.xdg.dataHome}/cargo\n
+      export _JAVA_OPTIONS=-Djava.util.prefs.userRoot=${config.xdg.configHome}/java\n
       export GRADLE_USER_HOME=${config.xdg.dataHome}/gradle\n
-      export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+      export NPM_CONFIG_USERCONFIG=${config.xdg.configHome}/npm/npmrc\n
+      export KUBECONFIG=${config.xdg.configHome}/kube\n
+      export KUBECACHEDIR=${config.xdg.cacheHome}/kube\n
+      export GOPATH=${config.xdg.dataHome}/go\n
+      export GOMODCACHE=${config.xdg.cacheHome}/go/mod\n
+      export DOCKER_CONFIG=${config.xdg.configHome}/docker\n
+      export ANSIBLE_HOME=${config.xdg.configHome}/ansible\n
+      export ANSIBLE_CONFIG=${config.xdg.configHome}/ansible.cfg\n
+      export ANSIBLE_GALAXY_CACHE_DIR=${config.xdg.cacheHome}/ansible/galaxy_cache\n
+      export VAGRANT_HOME=${config.xdg.dataHome}/vagrant\n
+      export VAGRANT_ALIAS_FILE=${config.xdg.dataHome}/vagrant/aliases\n
+      export GTK2_RC_FILES=${config.xdg.configHome}/gtk-2.0/gtkrc:${config.xdg.configHome}/gtk-2.0/gtkrc.mine\n
+      export XCOMPOSEFILE=${config.xdg.configHome}/X11/xcompose\n
+      export XCOMPOSECACHE=${config.xdg.cacheHome}/X11/xcompose
     ";
     # extra commands to be added to .zprofile
     #profileExtra = "[[ \"$(tty)\" == \"/dev/tty1\" ]] && exec Hyprland";
