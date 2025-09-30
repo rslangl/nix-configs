149027376074281bfba70f563c568bc364e8beb0
rslangl <fjellape1447@protonmail.com>
2025-09-30 21:11:45 +0200

    fix: keychain integration

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -29,6 +29,7 @@
       sbt = "sbt -ivy $XDG_DATA_HOME/ivy2 -sbt-dir $XDG_CONFIG_HOME/sbt";
       mvn = "mvn -gs $XDG_CONFIG_HOME/maven/settings.xml";
       nvidia-settings = "nvidia-settings --config=$XDG_CONFIG_HOME/nvidia/settings";
+      keychain = "keychain --absolute --dir $XDG_RUNTIME_DIR/keychain";
     };
     dirHashes = {
       dev = "$HOME/dev";
@@ -74,7 +75,7 @@
     # extra commands to be added to .zshrc
     initContent = "
       eval \"$(zoxide init zsh)\" > /dev/null 2>&1\n
-      eval \"$(keychain --absolute --dir $XDG_RUNTIME_DIR/keychain --eval ssh $HOME/.ssh/github --quiet)\"
+      eval \"$(keychain --eval github --quiet)\"
     ";
     oh-my-zsh = {
       enable = true;
