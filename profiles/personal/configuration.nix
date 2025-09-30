fbca5f85a5255eecd86f91b0a070ea88e154fce3
rslangl <fjellape1447@protonmail.com>
2025-09-30 20:47:58 +0200

    fix: lint

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -61,13 +61,13 @@
   };
 
   environment.variables = rec {
-    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME   = "$HOME/.local/share";
-    XDG_STATE_HOME  = "$HOME/.local/state";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
 
-    XDG_BIN_HOME    = "$HOME/.local/bin";
-    PATH = [ 
+    XDG_BIN_HOME = "$HOME/.local/bin";
+    PATH = [
       "${XDG_BIN_HOME}"
     ];
   };
