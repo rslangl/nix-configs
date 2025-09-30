aaa0ae9e0715e6323c6dcbe6a382cc28762ee40c
rslangl <fjellape1447@protonmail.com>
2025-09-30 19:58:51 +0200

    fix: use correct config key for system-level config

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -60,7 +60,7 @@
     uid = 1000;
   };
 
-  environment.sessionVariables = rec {
+  environment.variables = rec {
     XDG_CACHE_HOME  = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_HOME   = "$HOME/.local/share";
