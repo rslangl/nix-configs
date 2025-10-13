1b3b49672787c643af141cdbac52c8978872869a
user <mailman@kek.net>
2025-10-13 20:06:47 +0200

    fix: firefox bookmarks

--- user/app/browser/firefox.nix
+++ user/app/browser/firefox.nix
@@ -8,36 +8,33 @@ _: {
       default = {
         id = 0;
         name = "default";
-        bookmarks = [
-          {
-            name = "searx";
-            tags = ["search" "searx"];
-            keyword = "searx";
-            url = "https://metasearx.com";
-          }
-          {
-            name = "wikipedia";
-            tags = ["wiki"];
-            keyword = "wiki";
-            url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-          }
-          {
-            name = "Nix";
-            toolbar = true;
-            bookmarks = [
-              {
-                name = "wiki";
-                tags = ["wiki" "nix"];
-                url = "https://wiki.nixos.org/";
-              }
-              {
-                name = "packages";
-                tags = ["packages" "nix"];
-                url = "https://search.nixos.org/packages";
-              }
-            ];
-          }
-        ];
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "searx";
+              tags = ["search" "searx"];
+              keyword = "searx";
+              url = "https://metasearx.com";
+            }
+            {
+              name = "Nix";
+              toolbar = true;
+              bookmarks = [
+                {
+                  name = "wiki";
+                  tags = ["wiki" "nix"];
+                  url = "https://wiki.nixos.org/";
+                }
+                {
+                  name = "packages";
+                  tags = ["packages" "nix"];
+                  url = "https://search.nixos.org/packages";
+                }
+              ];
+            }
+          ];
+        };
         # extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         #   ublock-origin
         #   privacy-badger
